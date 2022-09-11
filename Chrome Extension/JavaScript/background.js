5fbfc4c9c7fe999f2221b369d5faf07a1dfb5181
Cristian Gutierrez <Cristiang52001@hotmail.com>
2022-09-11 00:32:19 -0400

    nevermind..

--- Chrome Extension/JavaScript/background.js
+++ Chrome Extension/JavaScript/background.js
@@ -1,5 +1,5 @@
 let color= "#3AAA757";
 chrome.runtime.onInstalled.addListener(() => {
    chrome.storage.sync.set ({ color });
-   console.log (`Defualt background logo set to ${color}`); 
+   console.log (`Default background logo set to ${color}`); 
 });
