67fcba3dea43150de748ba5e29e16f1903c2fc6c
Cristian Gutierrez <Cristiang52001@hotmail.com>
2022-09-10 20:00:55 -0400

    added more color js

--- Chrome Extension/JavaScript/iconChoice.js
+++ Chrome Extension/JavaScript/iconChoice.js
@@ -1,5 +1,5 @@
 let color= "#3AAA757";
 chrome.runtime.onInstalled.addListener(() => {
    chrome.storage.sync.set ({ color });
-   console.log (`Defualt background color set to ${color}`); 
+   console.log (`Defualt background logo set to ${color}`); 
 });
